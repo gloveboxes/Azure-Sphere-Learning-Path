3c80a5a08f703851ca2f07ae336e7734aaaa93be
Dave Glover <dglover@microsoft.com>
2020-01-28 21:32:36 +1100

    doco

--- Lab 1 - Build your first Azure Sphere Application with Visual Studio/Azure_Sphere_plus_Grove_Shield_Sensor_Kit/main.c
+++ Lab 1 - Build your first Azure Sphere Application with Visual Studio/Azure_Sphere_plus_Grove_Shield_Sensor_Kit/main.c
@@ -44,12 +44,13 @@ int main(int argc, char* argv[])
 	GroveShield_Initialize(&i2cFd, 115200);
 	void* sht31 = GroveTempHumiSHT31_Open(i2cFd);
 
-
 	while (!terminationRequested) {
 
 		GroveTempHumiSHT31_Read(sht31);
+
 		float temp = GroveTempHumiSHT31_GetTemperature(sht31);
 		float humi = GroveTempHumiSHT31_GetHumidity(sht31);
+
 		Log_Debug("Temperature: %.1fC\n", temp);
 		Log_Debug("Humidity: %.1f\%c\n", humi, 0x25);
 
