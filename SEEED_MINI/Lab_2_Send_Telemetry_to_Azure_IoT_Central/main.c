3debf51334a55424112fd65ab876d20ec1969895
Dave Glover <dglover@microsoft.com>
2020-04-10 19:24:28 +1000

    InitPeripheralsAndHandlers return now void

--- SEEED_MINI/Lab_2_Send_Telemetry_to_Azure_IoT_Central/main.c
+++ SEEED_MINI/Lab_2_Send_Telemetry_to_Azure_IoT_Central/main.c
@@ -16,7 +16,7 @@
 #define JSON_MESSAGE_BYTES 128  // Number of bytes to allocate for the JSON telemetry message for IoT Central
 
 // Forward signatures
-static int InitPeripheralsAndHandlers(void);
+static void InitPeripheralsAndHandlers(void);
 static void ClosePeripheralsAndHandlers(void);
 static void Led1BlinkHandler(EventLoopTimer* eventLoopTimer);
 static void Led2OffHandler(EventLoopTimer* eventLoopTimer);
@@ -77,9 +77,7 @@ int main(int argc, char* argv[]) {
 		return ExitCode_Missing_ID_Scope;
 	}
 
-	if (InitPeripheralsAndHandlers() != 0) {
-		return ExitCode_Init_Failed;
-	}
+	InitPeripheralsAndHandlers();
 
 	// Main loop
 	while (!IsTerminationRequired()) {
@@ -243,14 +241,12 @@ static void Led1BlinkHandler(EventLoopTimer* eventLoopTimer) {
 ///  Initialize peripherals, device twins, direct methods, timers.
 /// </summary>
 /// <returns>0 on success, or -1 on failure</returns>
-static int InitPeripheralsAndHandlers(void) {
+static void InitPeripheralsAndHandlers(void) {
 	InitializeDevKit();
 
 	OpenPeripheralSet(peripheralSet, NELEMS(peripheralSet));
 
 	StartTimerSet(timerSet, NELEMS(timerSet));
-
-	return 0;
 }
 
 /// <summary>
