6dc91ede523a3b6c60990c88750ced6c117b658e
Dave Glover <dglover@microsoft.com>
2020-08-11 10:52:47 +1000

    Check for Direct Methof NULL responseMsg

--- LearningPathLibrary/direct_methods.c
+++ LearningPathLibrary/direct_methods.c
@@ -3,12 +3,14 @@
 static LP_DIRECT_METHOD_BINDING** _directMethods;
 static size_t _directMethodCount;
 
-void lp_openDirectMethodSet(LP_DIRECT_METHOD_BINDING* directMethods[], size_t directMethodCount) {
+void lp_openDirectMethodSet(LP_DIRECT_METHOD_BINDING* directMethods[], size_t directMethodCount)
+{
 	_directMethods = directMethods;
 	_directMethodCount = directMethodCount;
 }
 
-void lp_closeDirectMethodSet(void) {
+void lp_closeDirectMethodSet(void)
+{
 	_directMethods = NULL;
 	_directMethodCount = 0;
 }
@@ -17,7 +19,8 @@ void lp_closeDirectMethodSet(void) {
 This implementation of Direct Methods expects a JSON Payload Object
 */
 int lp_azureDirectMethodHandler(const char* method_name, const unsigned char* payload, size_t payloadSize,
-	unsigned char** responsePayload, size_t* responsePayloadSize, void* userContextCallback) {
+	unsigned char** responsePayload, size_t* responsePayloadSize, void* userContextCallback)
+{
 
 	const char* methodSucceededMsg = "Method Succeeded";
 	const char* methodNotFoundMsg = "Method not found";
@@ -43,7 +46,8 @@ int lp_azureDirectMethodHandler(const char* method_name, const unsigned char* pa
 	*responsePayloadSize = 0; // Response payload content size.
 
 	char* payLoadString = (char*)malloc(payloadSize + 1);
-	if (payLoadString == NULL) {
+	if (payLoadString == NULL)
+	{
 		responseMessage = mallocFailedMsg;
 		result = LP_METHOD_FAILED;
 		goto cleanup;
@@ -53,28 +57,33 @@ int lp_azureDirectMethodHandler(const char* method_name, const unsigned char* pa
 	payLoadString[payloadSize] = 0; //null terminate string
 
 	root_value = json_parse_string(payLoadString);
-	if (root_value == NULL) {
+	if (root_value == NULL)
+	{
 		responseMessage = invalidJsonMsg;
 		result = LP_METHOD_FAILED;
 		goto cleanup;
 	}
 
 	jsonObject = json_value_get_object(root_value);
-	if (jsonObject == NULL) {
+	if (jsonObject == NULL)
+	{
 		responseMessage = invalidJsonMsg;
 		result = LP_METHOD_FAILED;
 		goto cleanup;
 	}
 
 	// loop through array of DirectMethodBindings looking for a matching method name
-	for (int i = 0; i < _directMethodCount; i++) {
-		if (strcmp(method_name, _directMethods[i]->methodName) == 0) {
+	for (int i = 0; i < _directMethodCount; i++)
+	{
+		if (strcmp(method_name, _directMethods[i]->methodName) == 0)
+		{
 			directMethodBinding = _directMethods[i];
 			break;
 		}
 	}
 
-	if (directMethodBinding != NULL && directMethodBinding->handler != NULL) {	// was a LP_DIRECT_METHOD_BINDING found
+	if (directMethodBinding != NULL && directMethodBinding->handler != NULL)
+	{	// was a LP_DIRECT_METHOD_BINDING found
 
 		responseCode = directMethodBinding->handler(jsonObject, directMethodBinding, &responseMsg);
 
@@ -83,10 +92,10 @@ int lp_azureDirectMethodHandler(const char* method_name, const unsigned char* pa
 		switch (responseCode)
 		{
 		case LP_METHOD_SUCCEEDED:	// 200
-			responseMessage = strlen(responseMsg) == 0 ? methodSucceededMsg : responseMsg;
+			responseMessage = responseMsg == NULL || strlen(responseMsg) == 0 ? methodSucceededMsg : responseMsg;
 			break;
 		case LP_METHOD_FAILED:		// 500
-			responseMessage = strlen(responseMsg) == 0 ? methodErrorMsg : responseMsg;
+			responseMessage = responseMsg == NULL || strlen(responseMsg) == 0 ? methodErrorMsg : responseMsg;
 			break;
 		case LP_METHOD_NOT_FOUND:
 			break;
@@ -101,30 +110,33 @@ cleanup:
 	*responsePayloadSize = responseMessageLength + 2; // add two as going to wrap the message with quotes for JSON
 
 	*responsePayload = (unsigned char*)malloc(*responsePayloadSize);
-	if (*responsePayload != NULL) {
+	if (*responsePayload != NULL)
+	{
 		// response message needs to be wrapped in quotes as it is part of the JSON payload object
 		memcpy(*responsePayload, "\"", 1);
 		memcpy(*responsePayload + 1, responseMessage, responseMessageLength);
 		memcpy(*responsePayload + responseMessageLength + 1, "\"", 1);
 	}
-	else {
+	else
+	{
 		*responsePayloadSize = 0;
 	}
 
-	if (root_value != NULL) {
+	if (root_value != NULL)
+	{
 		json_value_free(root_value);
 	}
 
-	if (payLoadString != NULL) {
+	if (payLoadString != NULL)
+	{
 		free(payLoadString);
 		payLoadString = NULL;
 	}
 
-	if (directMethodBinding != NULL) {
-		if (responseMsg != NULL) { // there was memory allocated for a response message so free it now
-			free(responseMsg);
-			responseMsg = NULL;
-		}
+	if (responseMsg != NULL)
+	{ // there was memory allocated for a response message so free it now
+		free(responseMsg);
+		responseMsg = NULL;
 	}
 
 	return result;
