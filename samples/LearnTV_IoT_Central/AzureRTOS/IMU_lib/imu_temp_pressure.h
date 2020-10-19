80dea976374602e1883d28190f8598350e35718f
Dave Glover <dglover@microsoft.com>
2020-10-19 16:06:44 +1100

    IMU realtime drivers

---
+++ samples/LearnTV_IoT_Central/AzureRTOS/IMU_lib/imu_temp_pressure.h
@@ -0,0 +1,44 @@
+#pragma once
+
+// #include "hw/azure_sphere_learning_path.h"
+#include "lsm6dso_reg.h"
+#include "lps22hh_reg.h"
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "tx_api.h"
+#include "os_hal_i2c.h"
+
+#define LSM6DSO_ADDRESS	   0x6A	  // I2C Address
+static uint8_t i2cHandle = OS_HAL_I2C_ISU2;
+static const uint8_t i2c_speed = I2C_SCL_1000kHz;
+
+#define I2C_MAX_LEN 64
+static uint8_t i2c_tx_buf[I2C_MAX_LEN];
+static uint8_t i2c_rx_buf[I2C_MAX_LEN];
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} AngularRateDegreesPerSecond;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} AccelerationMilligForce;
+
+void lp_imu_initialize(void);
+void lp_imu_close(void);
+float lp_get_temperature(void);
+float lp_get_pressure(void);
+float lp_get_temperature_lps22h(void);	// get_temperature() from lsm6dso is faster
+void lp_calibrate_angular_rate(void);
+AngularRateDegreesPerSecond lp_get_angular_rate(void);
+AccelerationMilligForce lp_get_acceleration(void);
